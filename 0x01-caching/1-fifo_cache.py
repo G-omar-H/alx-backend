74617cb999bd9655175e0eda1224d19822f9ee62
G-omar-H <omarghazi12@gmail.com>
2024-06-27 00:42:56 +0100

    1. FIFO caching mandatory

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -11,23 +11,6 @@ class FIFOCache(BaseCaching):
     class FIFOCache that inherits from BaseCaching and is a caching system:
 
     You must use self.cache_data - dictionary from the parent class BaseCaching
-    def put(self, key, item):
-        Must assign to the dictionary self.
-            cache_data the item value for the key key.
-        If key or item is None, this method should not do anything.
-        If the number of items in self.
-            cache_data is higher that BaseCaching.MAX_ITEMS:
-            you must discard the first item put in cache (FIFO algorithm)
-            you must print DISCARD: with the key discarded
-                and following by a new line
-    def get(self, key):
-        Must return the value in self.cache_data linked to key.
-        If key is None or if the key doesn't exist in self.
-            cache_data, return None.
-
-
-    Args:
-        BaseCaching (_type_): _description_
     """
 
     def __init__(self):
@@ -38,38 +21,26 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """
-        Must assign to the dictionary self.
-            cache_data the item value for the key key.
+        Must assign to the dictionary self.cache_data the item value fo
         If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher
-            that BaseCaching.MAX_ITEMS:
-            you must discard the first item put in cache (FIFO algorithm)
-            you must print DISCARD: with the key discarded
-                and following by a new line
-
-        Args:
-            key (_type_): _description_
-            item (_type_): _description_
+        If the number of items in self.cache_data is higher than BaseCachin
+        you must discard the first item put in cache (FIFO algorithm)
+        you must print DISCARD: with the key discarded and following
         """
         if key is None or item is None:
             return
-        if len(self.cache_data) ==\
-                BaseCaching.MAX_ITEMS\
-                and key not in self.cache_data.keys():
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
-            print(f"DISCARD: {first_key}")
+            print("DISCARD:", first_key)
             del self.cache_data[first_key]
         self.cache_data[key] = item
 
-        def get(self, key):
-            """
-            return the value in self.cache_data linked to key.
-            If key is None or if the key doesn't exist in self.
-                cache_data, return None.
-
-            Args:
-                key (_type_): _description_
-            """
-            if key is None or key not in self.cache_data.keys():
-                return None
-            return self.cache_data[key]
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+        If key is None or if the key doesn't exist in self.cache_data,
+        return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
