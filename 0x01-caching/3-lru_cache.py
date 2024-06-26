f94e0509abc9fc32ca377029857efd7e45430447
G-omar-H <omarghazi12@gmail.com>
2024-06-27 00:26:49 +0100

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,98 +1,40 @@
-#!/usr/bin/env python3
-"""
-3-lru_cache.py
-"""
+#!/usr/bin/python3
+""" LRU Caching """
 
-BaseCaching = __import__("base_caching").BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    class LRUCache that inherits from BaseCaching and is a caching system:
-
-    You must use self.cache_data - dictionary from the parent class BaseCaching
-    You can overload def __init__(self): but don't forget
-        to call the parent init: super().__init__()
-    def put(self, key, item):
-        Must assign to the dictionary self.cache_data
-            the item value for the key key.
-        If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data is higher that
-            BaseCaching.MAX_ITEMS:
-            you must discard the least recently used item (LRU algorithm)
-            you must print DISCARD: with the key discarded
-                and following by a new line
-    def get(self, key):
-        Must return the value in self.cache_data linked to key.
-        If key is None or if the key doesn't exist in
-        self.cache_data, return None.
-
-    Args:
-        BaseCaching (_type_): _description_
-    """
+    """ LRU Cache """
 
     def __init__(self):
-        """
-        initializer
-        """
-        self.lru = None
-        self.cache_list = list()
-        self.ageDict = dict()
+        """ Initialize """
         super().__init__()
-
-    def set_age(self):
-        """
-        track the age of each cache
-        """
-        for key in self.cache_data.keys():
-            if key not in self.ageDict.keys():
-                self.ageDict[key] = 0
-
-    def set_lru(self):
-        """
-        define the lest recentely used cache
-        """
-        a, b = next(iter(self.ageDict.items()))
-        for key, value in self.ageDict.items():
-            if b > value:
-                a = key
-                b = value
-        self.lru = a
+        self.order = []
 
     def put(self, key, item):
-        """
-        Must assign to the dictionary self.cache_data
-        the item value for the key key.
-        If key or item is None, this method should not do anything.
-        If the number of items in self.cache_data
-            is higher that BaseCaching.MAX_ITEMS:
-
-        must discard the least recently used item (LRU algorithm)
-        must print DISCARD: with the key discarded and following by a new
-        """
+        """ Add an item in the cache """
+        print(self.order)
         if key is None or item is None:
             return
-        self.set_age()
-        if len(
-                self.cache_data) == self.MAX_ITEMS and key not in self.cache_data.keys():
-            self.set_lru()
-            print(f"DISCARD: {self.lru}")
-            del self.cache_data[self.lru]
-            del self.ageDict[self.lru]
+
+        if key in self.cache_data:
+            self.order.remove(key)
+
         self.cache_data[key] = item
-        self.ageDict[key] = 0
-        print(f"                               {self.ageDict}")
+        self.order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru_key = self.order.pop(0)
+            del self.cache_data[lru_key]
+            print("DISCARD:", lru_key)
 
     def get(self, key):
-        """
-        return the value in self.cache_data linked to key.
-        If key is None or if the key doesn't exist in
-        self.cache_data, return None.
-
-        Args:
-            key (_type_): _description_
-        """
-        if key is None or key not in self.cache_data.keys():
+        """ Get an item from the cache """
+        if key is None or key not in self.cache_data:
             return None
-        self.ageDict[key]: self.ageDict[key] = + 1
+
+        self.order.remove(key)
+        self.order.append(key)
+
         return self.cache_data[key]
